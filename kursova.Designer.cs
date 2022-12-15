138396730664e520a57a3239719809b0c90db4d2
Krisi2312 <114053531+Krisi2312@users.noreply.github.com>
2022-12-15 17:26:32 +0200

    Add files via upload

---
+++ kursova.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace kursova_rabota_hristozova
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblright = new System.Windows.Forms.Label();
+            this.lblleft = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxright = new System.Windows.Forms.TextBox();
+            this.textBoxleft = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Колко числа ще въвеждаш от двете страни";
+            // 
+            // lblright
+            // 
+            this.lblright.AutoSize = true;
+            this.lblright.Location = new System.Drawing.Point(432, 76);
+            this.lblright.Name = "lblright";
+            this.lblright.Size = new System.Drawing.Size(75, 13);
+            this.lblright.TabIndex = 1;
+            this.lblright.Text = "дясна страна";
+            // 
+            // lblleft
+            // 
+            this.lblleft.AutoSize = true;
+            this.lblleft.Location = new System.Drawing.Point(49, 76);
+            this.lblleft.Name = "lblleft";
+            this.lblleft.Size = new System.Drawing.Size(69, 13);
+            this.lblleft.TabIndex = 2;
+            this.lblleft.Text = "лява страна";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(282, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBoxright
+            // 
+            this.textBoxright.Location = new System.Drawing.Point(362, 101);
+            this.textBoxright.Name = "textBoxright";
+            this.textBoxright.Size = new System.Drawing.Size(199, 20);
+            this.textBoxright.TabIndex = 4;
+            // 
+            // textBoxleft
+            // 
+            this.textBoxleft.Location = new System.Drawing.Point(24, 101);
+            this.textBoxleft.Name = "textBoxleft";
+            this.textBoxleft.Size = new System.Drawing.Size(197, 20);
+            this.textBoxleft.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkRed;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(64, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Проверка";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "изход:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::kursova_rabota_hristozova.Properties.Resources.photo_hristozova;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 290);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(583, 94);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(82, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(82, 190);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(82, 229);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "label9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(591, 396);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxleft);
+            this.Controls.Add(this.textBoxright);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblleft);
+            this.Controls.Add(this.lblright);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Проверка на числа";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblright;
+        private System.Windows.Forms.Label lblleft;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxright;
+        private System.Windows.Forms.TextBox textBoxleft;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ kursova.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kursova_rabota_hristozova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            var textNumLeft = textBoxleft.Text.Select(i => Convert.ToInt32(textBoxleft.Text)).ToArray();
+            */
+            int textNumLeft = Convert.ToInt32(textBoxleft.Text);
+            int textNumRight = Convert.ToInt32(textBoxright.Text);
+
+            if (textBoxleft.Text.Length == 0 || textBoxright.Text.Length == 0)
+            {
+                label5.Text = "въвел си едно въвеждане";
+
+            }
+            else
+            {
+                label8.Text = " колко броя въвеждания:" +textBox1.Text ;
+
+                label6.Text = " лява страна: " + textBoxleft.Text;
+               
+                
+                label7.Text = " дясна страна: " + textBoxright.Text;
+
+                if (textNumLeft != textNumRight)
+                {
+                    label9.Text = textBoxleft.Text + " не е " + textBoxright.Text + " не са равни двете страни";
+                }
+                
+                else 
+                {
+                    label9.Text = textBoxleft.Text + " e " + textBoxright.Text + " двете страни са равни";
+                }
+               
+
+            }
+            {
+                
+            }
+            // dva teksboksa (tekstovete) da gi preverna v int s int parse i da gi sravnq
+
+        }
+    }
+}
